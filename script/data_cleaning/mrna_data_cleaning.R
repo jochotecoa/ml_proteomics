3dc914ad00117843b5508e73ab53474cb3e7a536
Juan Ochoteco <jochotecoa@gmail.com>
2021-03-16 11:21:18 +0100

    16-Mar-21

--- script/data_cleaning/mrna_data_cleaning.R
+++ script/data_cleaning/mrna_data_cleaning.R
@@ -1,4 +1,5 @@
 source('script/functions/functions_JOA.R')
+forceLibrary(c('dplyr', 'tibble', 'biomaRt'))
 
 mrna_dir = '/share/analysis/hecatos/juantxo/mRNA/quant_salmon/Homo_sapiens.GRCh38.cdna.ncrna.circbase/'
 tissue = 'cardiac'
@@ -6,4 +7,42 @@ compound = 't0_controls'
 
 mrna_dir_spe = paste0(mrna_dir, tissue, '/', compound)
 
-mrna_df = mergeFiles(files_patt = 'quant.sf', by_col = 'Name', path = mrna_dir_spe, all_true = T, recursive = T)
+mrna_df = mergeFiles(files_patt = 'quant.sf', by_col = 'Name', path = mrna_dir_spe, all_true = T, recursive = T, header = T)
+
+colnames(mrna_df) = colnames(mrna_df) %>% 
+  gsub(pattern = '/share/analysis/hecatos/juantxo/mRNA/quant_salmon/Homo_sapiens.GRCh38.cdna.ncrna.circbase/cardiac/t0_controls/', replacement = '') %>% 
+  gsub(pattern = '/quant.sf|_quant|TPM', replacement = '')
+
+mrna_df = mrna_df %>% 
+  column_to_rownames('Name') %>% 
+  select(contains('TPM'))
+
+rownames(mrna_df) = rownames(mrna_df) %>% 
+  gsub('\\..*', '', .) 
+
+mart = openMart2018()
+
+mrna_prot_ids = getBM(attributes = c('ensembl_transcript_id', 'uniprot_gn'), 
+                      filters = 'ensembl_transcript_id', 
+                      values = rownames(mrna_df), 
+                      mart = mart)
+
+mrna_unip_df = mrna_df %>% 
+  rownames_to_column('ensembl_transcript_id') %>% 
+  merge.data.frame(y = mrna_prot_ids, by = 'ensembl_transcript_id', all.y = T)
+
+### Divide transcripts per protein (after connecting to protein expression data)
+
+dupl_mrna = mrna_unip_df$ensembl_transcript_id %>% 
+  subset(., duplicated(.))
+
+dupl_mrna = mrna_unip_df$ensembl_transcript_id[mrna_unip_df$ensembl_transcript_id %in% dupl_mrna] %>% 
+  table() %>% 
+  as.data.frame()
+
+colnames(dupl_mrna) = c('ensembl_transcript_id', 'n_protein_per_transcript')
+
+mrna_unip_df = mrna_unip_df %>% 
+  merge.data.frame(y = dupl_mrna, all = T)
+
+mrna_unip_df$n_protein_per_transcript[is.na(mrna_unip_df$n_protein_per_transcript)] = 1
