4f0196422fd50ffa02b9285981997c73c81feb66
Juan Ochoteco <jochotecoa@gmail.com>
2022-03-03 00:58:37 +0100

    03-Mar-22

--- script/go_terms_analysis/rsquared_on_different_go_terms.R
+++ script/go_terms_analysis/rsquared_on_different_go_terms.R
@@ -38,9 +38,77 @@ rfFit <- train(proteomics_value ~ .,
 
 # rfFit = readRDS(file = 'output/cardiac/na_omit/split_by_sample/72_features/rfFit.rds')
 
-pred = predict(rfFit, dataset_sample)
-obsVSpred = dataset_sample$proteomics_value %>% 
+pred = predict(rfFit, dataset_test)
+obsVSpred = dataset_test$proteomics_value %>% 
   cbind.data.frame(obs = ., pred)
 
 
-a = cor(obsVSpred[1], obsVSpred[2])
+rsquared_all_prot = (cor(obsVSpred[1], obsVSpred[2]))^2
+
+obsVSpred$uniprotswissprot = obsVSpred %>% rownames() %>% strsplit('--') %>% 
+  sapply('[[', 1)
+
+
+library(biomaRt)
+
+mart=useMart(biomart="ensembl", dataset="hsapiens_gene_ensembl")
+
+
+prot_goterms = getBM(attributes = c('name_1006', 'uniprotswissprot'), 
+                     filters = 'uniprotswissprot', 
+                     values = obsVSpred$uniprotswissprot, 
+                     mart = mart)
+
+populated_goterms = table(prot_goterms$name_1006) >= 10
+populated_goterms = populated_goterms[populated_goterms] %>% 
+  names()
+
+
+obsVSpred_goterms = obsVSpred %>% 
+  merge.data.frame(prot_goterms, by = 'uniprotswissprot')
+
+rsquared_go_results = data.frame()
+
+# goterm = NULL, rsquared_goterm = NULL
+for (goterm in populated_goterms) {
+  obsVSpred_goterm = obsVSpred_goterms[obsVSpred_goterms$name_1006 == goterm, ]
+  
+  rsquared_goterm = (cor(obsVSpred_goterm[2], obsVSpred_goterm[3]))^2
+  
+  rsquared_go_result = data.frame(goterm, rsquared_goterm)
+  
+  rsquared_go_results = rsquared_go_results %>% 
+    rbind(rsquared_go_result)
+  
+}
+
+rsquared_go_results$rsquared = rsquared_go_results$pred
+
+worst_goterms = rsquared_go_results[order(rsquared_go_results$rsquared), ] %>% head()
+best_goterms = rsquared_go_results[order(rsquared_go_results$rsquared), ] %>% tail()
+barplot_goterms = rbind(worst_goterms, best_goterms)
+
+
+pdf()
+
+par(mar=c(4,14,4,4))
+barplot(barplot_goterms$rsquared, 
+        names.arg = d$goterm, 
+        horiz = T, 
+        las =2, 
+        xlim = c(0, 1), 
+        main = 'Best and worse GO terms by Rsquared')# rsquared_go_results %<>% na.omit()
+
+
+dev.off()
+
+tiff("output/plots/best_worse_goterms.tiff", units="in", width=10, height=5, res=300)
+par(mar=c(4,14,4,8))
+barplot(barplot_goterms$rsquared, 
+        names.arg = d$goterm, 
+        horiz = T, 
+        las =2, 
+        xlim = c(0, 1), 
+        main = 'Best and worse predicted proteins grouped by GO terms (Rsquared)')# rsquared_go_results %<>% na.omit()
+
+dev.off()
